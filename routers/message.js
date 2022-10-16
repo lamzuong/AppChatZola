8449b897c97bf70316fd62745407cf9bdc29b5ce
lamzuong <lamdavid821@gmail.com>
2022-10-16 19:56:09 +0700

    aloha

--- routers/message.js
+++ routers/message.js
@@ -1,50 +1,49 @@
 const router = require('express').Router();
 const dotenv = require('dotenv');
-const {v4: uuid} = require('uuid');
+const { v4: uuid } = require('uuid');
 const docClient = require('../db.config');
 
 const tableName = 'message';
 
 router.post('/', (req, res) => {
-    const {conversationID, senderID, mess} = req.body
+    const { conversationID, sender, mess } = req.body;
     const params = {
         TableName: tableName,
         Item: {
             id: uuid(),
             conversationID,
-            senderID,
+            sender,
             mess,
-            date: new Date().getTime()
-        }
-    }
+            date: new Date().getTime(),
+        },
+    };
     docClient.put(params, (err, data) => {
-        if(err) {
-            return res.status(500).json("Loi: "+err);
+        if (err) {
+            return res.status(500).json('Loi: ' + err);
         }
-        console.log(data)
-        return res.status(200).json(data)
-    })
-})
+        console.log(data);
+        return res.status(200).json(data);
+    });
+});
 
 router.get('/:conversationID', (req, res) => {
-    const {conversationID} = req.params;
+    const { conversationID } = req.params;
     var params = {
         ExpressionAttributeValues: {
-         ":id": conversationID
-        }, 
-        ExpressionAttributeNames: {"#conversationID": "conversationID"},
-        FilterExpression: "contains(#conversationID , :id)", 
-        TableName: tableName
-       };
+            ':id': conversationID,
+        },
+        ExpressionAttributeNames: { '#conversationID': 'conversationID' },
+        FilterExpression: 'contains(#conversationID , :id)',
+        TableName: tableName,
+    };
     docClient.scan(params, (err, data) => {
-        if(err) {
+        if (err) {
             console.log(err);
-            return res.status(500).send("Loi"+err)
+            return res.status(500).send('Loi' + err);
+        } else {
+            return res.status(200).json(data.Items);
         }
-        else{
-            return res.status(200).json(data.Items)
-        }
-    })
-})
+    });
+});
 
 module.exports = router;
