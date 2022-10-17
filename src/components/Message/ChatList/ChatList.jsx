c9d16c812ae1065250a7d886d90b5cdb5518855c
nguyenminhvuong <minhvuong06082001@gmail.com>
2022-10-17 07:30:20 +0700

    search input

--- src/components/Message/ChatList/ChatList.jsx
+++ src/components/Message/ChatList/ChatList.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Fragment } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import { faXmarkCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './ChatList.module.scss';
-import Header from './Header/Header';
+import axiosCilent from '../../../api/axiosClient';
+
 import AccountItem from '../../AccountItem/AccountItem';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
@@ -20,32 +23,89 @@ const ChatList = (props) => {
     const sendData = (data) => {
         props.parentCb(data);
     };
+    const [text, setText] = useState('');
+    const [focus, setFocus] = useState(false);
+    const inputRef = useRef();
+    const handleCleanText = () => {
+        setText('');
+        inputRef.current.focus();
+    };
+
     const mess = props.data;
     const active = conversation.findIndex((e) => e.id === path);
+    const [result, setResultSearch] = useState([]);
+
     return (
         <div className={cx('wrapper')}>
             <div className={cx('header')}>
-                <Header />
-            </div>
-            <div className={cx('userOnl')}>
-                {mess.map((e, i) => (
-                    <AccountItem key={i} none={true} id={e.id} ava={e.ava} />
-                ))}
-            </div>
-            <ul className={cx('chatList')}>
-                {conversation.map((e, i) => (
-                    <div
-                        onClick={() => {
-                            sendData(e);
-                            navigate(`/t/${e.id}`);
-                        }}
-                    >
-                        <li key={i} className={cx(i === active ? 'active' : '')}>
-                            <Conversation key={i} id={e.id} conversation={e} currentUser={user} />
-                        </li>
+                {/* <Header /> */}
+                <div className={cx('wrapper-header')}>
+                    <div className={cx('container')}>
+                        <div className={cx('search')}>
+                            <div className={cx('btnSearch')}>
+                                <i className="bx bx-search"></i>
+                            </div>
+                            <input
+                                ref={inputRef}
+                                value={text}
+                                spellCheck={false}
+                                placeholder="Tìm kiếm"
+                                onChange={(e) => {
+                                    setText(e.target.value);
+                                }}
+                                onFocus={() => setFocus(true)}
+                            />
+                            {text && (
+                                <div className={cx('btnCleanText')} onClick={handleCleanText}>
+                                    <FontAwesomeIcon icon={faXmarkCircle} />
+                                </div>
+                            )}
+                        </div>
+                        <div className={cx('btnClose')} onClick={() => setFocus(false)}>
+                            Đóng
+                        </div>
                     </div>
-                ))}
-            </ul>
+                </div>
+            </div>
+            {focus ? (
+                <ul className={cx('chatList')}>
+                    {conversation.map((e, i) => (
+                        <div
+                            onClick={() => {
+                                sendData(e);
+                                navigate(`/t/${e.id}`);
+                            }}
+                        >
+                            <li key={i} className={cx(i === active ? 'active' : '')}>
+                                <Conversation key={i} id={e.id} conversation={e} currentUser={user} />
+                            </li>
+                        </div>
+                    ))}
+                </ul>
+            ) : (
+                <Fragment>
+                    <div className={cx('userOnl')}>
+                        {mess.map((e, i) => (
+                            <AccountItem key={i} none={true} id={e.id} ava={e.ava} />
+                        ))}
+                    </div>
+                    <ul className={cx('chatList')}>
+                        {conversation.map((e, i) => (
+                            <div
+                                onClick={() => {
+                                    sendData(e);
+                                    navigate(`/t/${e.id}`);
+                                }}
+                            >
+                                <li key={i} className={cx(i === active ? 'active' : '')}>
+                                    <Conversation key={i} id={e.id} conversation={e} currentUser={user} />
+                                </li>
+                            </div>
+                        ))}
+                    </ul>
+                </Fragment>
+            )}
+            {/* */}
         </div>
     );
 };
--- src/components/Message/ChatList/Header/Header.jsx
+++ src/components/Message/ChatList/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { faXmarkCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 import { useState, useRef } from 'react';
@@ -9,26 +11,33 @@ const cx = classNames.bind(styles);
 const Header = (props) => {
     const [text, setText] = useState('');
     const inputRef = useRef();
+    const handleCleanText = () => {
+        setText('');
+        inputRef.current.focus();
+    };
     return (
         <div className={cx('wrapper')}>
-            <h2>Trò chuyện</h2>
             <div className={cx('container')}>
                 <div className={cx('search')}>
+                    <div className={cx('btnSearch')}>
+                        <i className="bx bx-search"></i>
+                    </div>
                     <input
                         ref={inputRef}
                         value={text}
                         spellCheck={false}
-                        placeholder="Tim kiem"
+                        placeholder="Tìm kiếm"
                         onChange={(e) => {
                             setText(e.target.value);
                         }}
                     />
                     {text && (
-                        <div className={cx('btnSearch')}>
-                            <i className="bx bx-search"></i>
+                        <div className={cx('btnCleanText')} onClick={handleCleanText}>
+                            <FontAwesomeIcon icon={faXmarkCircle} />
                         </div>
                     )}
                 </div>
+                <div className={cx('btnClose')}>Đóng</div>
             </div>
         </div>
     );
